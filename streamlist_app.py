891035a152e01e4fc9c368dec8f2c0084a72a4a4
NoelWCatalyst <129048590+NoelWCatalyst@users.noreply.github.com>
2025-06-13 17:26:31 +0100

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-import pandas as ps
+import pandas as pd
 import numpy as nm
 import requests
 #from snowflake.snowpark.context import get_active_session
@@ -20,8 +20,14 @@ name_on_order = st.text_input('Name on smoothie?')
 st.write('The name on your smoothie will be: ', name_on_order)
 
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+#Convert the snowpark dataframe to a Pandas Datafrom so that we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 ingredients_list = st.multiselect (
     'Choose upto 5 ingredients:'
     , my_dataframe
@@ -35,7 +41,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-      st.stop()
+    st.stop()
 
     st.write(ingredients_string)
 
