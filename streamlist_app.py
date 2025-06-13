ce0741dbe8fa5af7f9163b95bbece878bdb31dec
NoelWCatalyst <129048590+NoelWCatalyst@users.noreply.github.com>
2025-06-13 16:51:39 +0100

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -46,5 +46,5 @@ if ingredients_list:
         st.success('Thanks ' + name_on_order + ', your smoothie is ordered!', icon="✅")
 #New section do display smoothiefroot nutrition information
 import requests
-smoothiefroot_response = request.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
